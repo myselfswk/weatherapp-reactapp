47638747dcb412b3cd039c68979dfa2685cb3ad8
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2021-08-28 00:04:03 +0500

    add icons
    add icons with git repository

--- src/App.js
+++ src/App.js
@@ -35,13 +35,52 @@ class App extends Component {
     Clouds: "wi-day-fog"
   }
 
+  // convert deg f to deg c
   calCelsius(temp) {
     let celsius = Math.floor(temp - 273.15);
     return celsius;
   }
 
   getWeatherIcon(icons, rangeID) {
-    
+    switch (true) {
+      case rangeID >= 200 && rangeID <= 232:
+        this.setState({
+          icon: this.weatherIcon.Thunderstorm
+        });
+        break;
+      case rangeID >= 300 && rangeID <= 321:
+        this.setState({
+          icon: this.weatherIcon.Drizzle
+        });
+        break;
+      case rangeID >= 500 && rangeID <= 531:
+        this.setState({
+          icon: this.weatherIcon.Rain
+        });
+        break;
+      case rangeID >= 600 && rangeID <= 622:
+        this.setState({
+          icon: this.weatherIcon.Snow
+        });
+        break;
+      case rangeID >= 701 && rangeID <= 781:
+        this.setState({
+          icon: this.weatherIcon.Atmosphere
+        });
+        break;
+      case rangeID === 800:
+        this.setState({
+          icon: this.weatherIcon.Clear
+        });
+        break;
+      case rangeID >= 801 && rangeID <= 804:
+        this.setState({
+          icon: this.weatherIcon.Clouds
+        });
+        break;
+      default:
+        break;
+    }
   }
 
   getWeather = async () => {
