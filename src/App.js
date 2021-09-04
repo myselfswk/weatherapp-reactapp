12f232f4c1d4717ebafd6aad6e12921d2d63b841
Muhammad Waleed Khan <fastnegative1004@gmail.com>
2021-09-05 04:09:48 +0500

    deploy web
    deploying web app to github

--- src/App.js
+++ src/App.js
@@ -91,7 +91,7 @@ class App extends Component {
 
     if (city && country) {
       //an api call to get data from the weather website
-      const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+      const api_call = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
 
       //catch response and convert it into json
       const response = await api_call.json();
